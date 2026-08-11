de8a2e0838c5cb161b41ba485916132c023f4a77
Sankari-26 <sankari.ug.23.cb@francisxavier.ac.in>
2026-08-11 04:36:51 +0000

    enter

---
+++ e1.pl
@@ -0,0 +1,22 @@
+% Parent facts
+parent(pam, bob).
+parent(tom, bob).
+parent(tom, liz).
+parent(bob, ann).
+parent(bob, pat).
+parent(pat, jim).
+
+% Who is male, who is female
+male(tom).
+male(bob).
+male(pat).
+male(jim).
+female(pam).
+female(liz).
+female(ann).
+
+% Rules
+sister(X, Y) :- female(X), parent(Z, X), parent(Z, Y).
+brother(X, Y) :- male(X), parent(Z, X), parent(Z, Y).
+grandma(X, Y) :- female(X), parent(X, Z), parent(Z, Y).
+grandpa(X, Y) :- male(X), parent(X, Z), parent(Z, Y).
